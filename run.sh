a31d8bfbfef97d6f113793d15a24af3bee0f6751
hckuo145 <hck0821@gmail.com>
2024-02-27 22:49:43 +0800

    run.sh (sinica)

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
-title=SEResNet34_base_MSTFT_ASP-HAD
-# title=SEResNet34_base_MSTFT_ASP-HAD-utter_only
-cmd="--title ${title} --model Spade --device cuda --batch 16  \
-     --model_conf config/model/SEResNet34_base_MSTFT_ASP.yaml \
-     --hyper_conf config/hyper/HAD.yaml                       \
-     --params exp/${title}/checkpoints/best_valid_loss.pt"
+# title=SEResNet34_base_MSTFT_ASP-HAD
+# cmd="--title ${title} --model Spade --device cuda --batch 16  \
+#      --model_conf config/model/SEResNet34_base_MSTFT_ASP.yaml \
+#      --hyper_conf config/hyper/HAD.yaml                       \
+#      --params exp/${title}/checkpoints/best_valid_loss.pt"
 
 
 # title=SEResNet34_base_MSTFT_ASP-Voicebox
@@ -14,7 +13,6 @@ cmd="--title ${title} --model Spade --device cuda --batch 16  \
 
 
 title=SEResNet34_base_MSTFT_ASP-Voicebox_v2
-# title=SEResNet34_base_MSTFT_ASP-Voicebox_v2-utter_only
 cmd="--title ${title} --model Spade --device cuda --batch 4   \
      --model_conf config/model/SEResNet34_base_MSTFT_ASP.yaml \
      --hyper_conf config/hyper/Voicebox_v2.yaml               \
